2e8af2ae1a8ae0345c1244c45151ae1806835709
Flavia <diama.m@macovei.eu>
2025-03-27 00:00:12 +0100

    implement data transformation
    lines are grouped by action (verb)
    output: [[action, [key, arg] *n, ...]

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from data.extract import extract
+from data.transform import transform
 
 def main():
     print("Hello from event-chain-mining!")
     raw_data = extract("data/data_files/preprocessed_short.txt")
-    print(raw_data)
+    transformed_data = transform(raw_data)
+    print(transformed_data)
 
 
 if __name__ == "__main__":
