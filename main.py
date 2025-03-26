30b79eb59a912d004bd550987ef37340d9522bf9
Flavia <diama.m@macovei.eu>
2025-03-26 23:58:53 +0100

    call data extraction in main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from data.extract import extract
+
 def main():
     print("Hello from event-chain-mining!")
+    raw_data = extract("data/data_files/preprocessed_short.txt")
+    print(raw_data)
 
 
 if __name__ == "__main__":
